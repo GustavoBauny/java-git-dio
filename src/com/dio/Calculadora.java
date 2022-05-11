dbe5b5b7f6bbeb4096868421c2f247de6d1f30fc
GustavoBauny <gustavogmartins03@gmail.com>
2022-05-11 16:31:12 -0300

    Modificação calculadora

--- src/com/dio/Calculadora.java
+++ src/com/dio/Calculadora.java
@@ -8,39 +8,39 @@ public class Calculadora {
 		   
         short operacao;
         double num1, num2;    
-        Scanner input = new Scanner(System.in);
-        
-        do {
-             System.out.println("Digite a operacao desejada:");
-             System.out.println("  1. Soma");      
-             System.out.println("  2. Subtracao");      
-             System.out.println("  3. Multiplicacao");      
-             System.out.println("  4. Divisao");      
-             System.out.println("  0. Sair");
-             System.out.print("Operacao: ");
-             operacao = input.nextShort();
-             
-             if (operacao == 0) {
-            	 System.out.println("Bye bye!");
-            	 break;
-             }
-             
-             if (!OperacaoExiste(operacao)) {
-            	 continue;
-             }
-             
-             System.out.print("Digite o primeiro valor: ");
-             num1 = input.nextDouble();
-             
-             System.out.print("Digite o segundo valor: ");
-             num2 = input.nextDouble();
-             
-             if (!ValidaDadosDeEntrada(operacao, num1, num2)) {
-            	 continue;
-             }
-        
-             System.out.println("RESULTADO: O resultado da operacao " + getNomeOperacao(operacao) + " eh " + Calcular(operacao, num1, num2) + "\n");
-        } while (operacao != 0);
+        try (Scanner input = new Scanner(System.in)) {
+			do {
+			     System.out.println("Digite a operacao desejada:");
+			     System.out.println("  1. Soma");      
+			     System.out.println("  2. Subtracao");      
+			     System.out.println("  3. Multiplicacao");      
+			     System.out.println("  4. Divisao");      
+			     System.out.println("  0. Sair");
+			     System.out.print("Operacao: ");
+			     operacao = input.nextShort();
+			     
+			     if (operacao == 0) {
+			    	 System.out.println("Bye bye!");
+			    	 break;
+			     }
+			     
+			     if (!OperacaoExiste(operacao)) {
+			    	 continue;
+			     }
+			     
+			     System.out.print("Digite o primeiro valor: ");
+			     num1 = input.nextDouble();
+			     
+			     System.out.print("Digite o segundo valor: ");
+			     num2 = input.nextDouble();
+			     
+			     if (!ValidaDadosDeEntrada(operacao, num1, num2)) {
+			    	 continue;
+			     }
+			
+			     System.out.println("RESULTADO: O resultado da operacao " + getNomeOperacao(operacao) + " eh " + Calcular(operacao, num1, num2) + "\n");
+			} while (operacao != 0);
+		}
     }
     
     static double Calcular (short operacao, double num1, double num2) {
---
+++ src/com/dio/exercicio1.java
@@ -0,0 +1,7 @@
+package com.dio;
+
+public class exercicio1 {
+    public static void main(String[] args){
+        
+    }
+}
