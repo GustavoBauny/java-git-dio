a5d4d6e8b00eff74b4738215fcfb08d7bb58d29b
GustavoBauny <gustavogmartins03@gmail.com>
2022-05-12 17:18:08 -0300

    Modificações exercícios

--- src/com/dio/ExerciciosLoop/Ex2_Nota.java
+++ src/com/dio/ExerciciosLoop/Ex2_Nota.java
@@ -1,6 +1,13 @@
 package com.dio.ExerciciosLoop;
 import java.util.Scanner;
 
+/*
+Faça um programa que peça uma nota, entre zero e dez.
+Mostre uma mensagem caso o valor seja inválido
+e continue pedindo
+até que o usuário informe um valor válido.
+*/
+
 public class Ex2_Nota {
     public static void main(String[] args) {
         try (Scanner scan = new Scanner(System.in)) {
