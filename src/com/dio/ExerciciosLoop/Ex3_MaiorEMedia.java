930e2fe5467f981a6fcc24b66a0f96a8e7c370b2
GustavoBauny <gustavogmartins03@gmail.com>
2022-05-12 17:10:51 -0300

    Terceiro exercicio loop

---
+++ src/com/dio/ExerciciosLoop/Ex3_MaiorEMedia.java
@@ -0,0 +1,26 @@
+package com.dio.ExerciciosLoop;
+import java.util.Scanner;
+
+public class Ex3_MaiorEMedia {
+    public static void main(String[] args){
+        try (Scanner scan = new Scanner(System.in)) {
+            int numero;
+            int maior = 0;
+            int soma = 0;
+            int count = 0;
+            do {
+                System.out.println("Digite o número: ");
+                numero = scan.nextInt();
+
+                soma = soma + numero;
+
+                if (numero > maior) maior = numero;
+
+                count = count + 1;
+            } while(count < 5);
+
+            System.out.println("O maior número é: " + maior);
+            System.out.println("A média é: " + (soma/5));
+        }
+    }
+}
