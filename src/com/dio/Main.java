88f52b112c956cdc04a51ccf5947dbb814fb7ef4
GustavoBauny <gustavogmartins03@gmail.com>
2022-05-12 13:40:04 -0300

    Adicionando atividade Quadrilatero

--- src/com/dio/Main.java
+++ src/com/dio/Main.java
@@ -1,11 +1,17 @@
 package com.dio;
 
-public class main {
+public class Main {
     public static void main(String args[]){
         System.out.println("Exercício empréstimo");
         teste.calcular(1000, teste.getDuasParcelas());
         teste.calcular(1000, teste.getTresParcelas());
         teste.calcular(1000, 5);  
+
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
     }
     
 }
