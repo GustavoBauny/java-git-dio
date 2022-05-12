d7e4aa196895fa4c7c0326d10032a151371c6e80
GustavoBauny <gustavogmartins03@gmail.com>
2022-05-12 13:18:12 -0300

    Exercicio emprestimo

---
+++ src/com/dio/main.java
@@ -0,0 +1,11 @@
+package com.dio;
+
+public class main {
+    public static void main(String args[]){
+        System.out.println("Exercício empréstimo");
+        teste.calcular(1000, teste.getDuasParcelas());
+        teste.calcular(1000, teste.getTresParcelas());
+        teste.calcular(1000, 5);  
+    }
+    
+}
