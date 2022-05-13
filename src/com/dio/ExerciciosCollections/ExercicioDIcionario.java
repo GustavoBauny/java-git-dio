205e69027b8c037228b277e585365e36e118b0e5
GustavoBauny <gustavogmartins03@gmail.com>
2022-05-13 15:25:03 -0300

    Adicionando exercícios de collection

---
+++ src/com/dio/ExerciciosCollections/ExercicioDIcionario.java
@@ -0,0 +1,14 @@
+package com.dio.ExerciciosCollections;
+
+/*Dadas as seguintes informações sobre meus livros favoritos e seus autores,
+crie um dicionário e ordene estes dicionário:
+exibindo (Nome Autor - Nome Livro);
+
+Autor = Stephen Hawking - Livro = nome: Uma Breve História do Tempo. páginas: 256
+Autor = Charles Duhigg - Livro = nome: O poder do hábito. páginas: 408
+Autor =  Yuval Noah Harari - Livro = nome: . páginas: 
+*/
+
+public class ExercicioDIcionario {
+    
+}
---
+++ src/com/dio/ExerciciosCollections/ExercicioLancamentodeDados.java
@@ -0,0 +1,41 @@
+package com.dio.ExerciciosCollections;
+
+import java.util.*;
+/*
+Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene.
+Depois, mostre quantas vezes cada valor foi conseguido
+*/
+
+public class ExercicioLancamentodeDados {
+    public static void main(String[] args) {
+        int quantLancamentos = 100;
+
+        List<Integer> valores = new ArrayList<>();
+        Random geradorDeLancamentos = new Random();
+        for (int i = 0; i < quantLancamentos; i++){
+            int number = geradorDeLancamentos.nextInt(6) + 1;
+            valores.add(number);
+        }
+
+        Map<Integer, Integer> lancamentos = new HashMap<>();
+        for (Integer resultado : valores){
+            if (lancamentos.containsKey(resultado))
+            lancamentos.put(resultado, (lancamentos.get(resultado) + 1));
+            else lancamentos.put(resultado, 1);
+        }
+
+        System.out.println("Jogando");
+        for (int i = 0; i < 3; i++){
+            try{
+                Thread.sleep(3000);
+                System.out.println(".");
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        System.out.println("\nValor "+" Quant. de vezes");
+        for (Map.Entry<Integer, Integer> entry : lancamentos.entrySet()){
+            System.out.printf("%3d %10d\n", entry.getKey(), entry.getValue());
+        }
+    }    
+}
