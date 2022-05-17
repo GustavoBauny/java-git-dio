c133066ded7be80cc846a6dc609be2e6848a2104
GustavoBauny <gustavogmartins03@gmail.com>
2022-05-17 14:51:42 -0300

    Modficiações Exercício Conceito

--- src/com/dio/Exercicios/Conceito.java
+++ src/com/dio/Exercicios/Conceito.java
@@ -9,24 +9,33 @@ Nota Conceito
 De 0,0 a 4,9 C
 De 5,0 a 8,9 B
 De 9,0 a 10,0 A
+
+Deverá apresentar a quantidade de alunos que tiveram conceito A, B e C.
 */
 
 public class Conceito {
     public static void main(String[] args) {
+        int contador1 = 0, contador2 = 0, contador3 = 0;
         try (Scanner scan = new Scanner(System.in)) {
             for (int i =1; i <= 10; i++){
                 System.out.println("Digite a nota final do aluno: ");
                 double nota = scan.nextDouble();
             if (nota <= 4.9){
                 System.out.println("O conceito final é C.");
+                contador1 += 1;
             }
             else if(nota <= 8.9 && nota >= 5){
                 System.out.println("O conceito final é B.");
+                contador2 += 1;
             }
             else{
                 System.out.println("O conceito final é A.");
+                contador3 += 1;
             }
             }
+            System.out.println("A quantidade de alunos que tiveram o conceito A é de: " + contador1);
+            System.out.println("A quantidade de alunos que tiveram o conceito B é de: " + contador2);
+            System.out.println("A quantidade de alunos que tiveram o conceito C é de: " + contador3);
         }
     }
 }
